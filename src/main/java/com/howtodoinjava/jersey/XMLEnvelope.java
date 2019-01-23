ba67e87907b859808a12f5190e1a83c780c89389
Bic Ed <devan.oppenheimer@uniposta.de>
2019-01-23 11:42:32 +0100

    xmlenvelop

--- src/main/java/com/howtodoinjava/jersey/XMLEnvelope.java
+++ src/main/java/com/howtodoinjava/jersey/XMLEnvelope.java
@@ -1,5 +1,11 @@
 package com.howtodoinjava.jersey;
 
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement(name = "xmlenvelop")
+@XmlAccessorType(XmlAccessType.FIELD)
 public class XMLEnvelope {
 	private String xmlbody;
 
