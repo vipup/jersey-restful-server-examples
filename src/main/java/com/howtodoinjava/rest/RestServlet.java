47ee74f24c6c875432f3c1cbf8d48bcc59d73f9b
Bic Ed <devan.oppenheimer@uniposta.de>
2019-01-28 14:26:49 +0100

    intro to REST-common servlet

---
+++ src/main/java/com/howtodoinjava/rest/RestServlet.java
@@ -0,0 +1,15 @@
+package com.howtodoinjava.rest;
+
+import com.sun.jersey.spi.container.servlet.ServletContainer;
+
+//com.sun.jersey.spi.container.servlet.
+//org.glassfish.jersey.servlet.ServletContainer
+public class RestServlet extends ServletContainer {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 3124679840668259416L;
+ 
+
+}
